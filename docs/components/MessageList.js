bb8e03e60ee9660baa68218b5eb9730f80258e12
sh-kang <shk9510@gmail.com>
2023-01-04 23:33:08 +0900

    Test

--- docs/components/MessageList.js
+++ docs/components/MessageList.js
@@ -1,6 +1,8 @@
-import MessageListItem from "./MessageListItem.js";
+import MessageListItem from './MessageListItem.js';
+// import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
 export default {
     name: 'MessageList',
+    // mixins: [lifecycleLogger],
     template: `<ul>
     <message-list-item v-for="item in items" :item="item" :key=item.id @delete="deleteMessage(item)"></message-list-item>
     </ul>`,
--- docs/components/MessageListItem.js
+++ docs/components/MessageListItem.js
@@ -1,6 +1,9 @@
+// import lifecycleLogger from '../mixins/lifecycle-logger.mixin.js';
 export default {
     name: 'MessageListItem',
-    template: `<li>{{ item.text }} = {{ item.createdAt }}
+    // mixins: [lifecycleLogger],
+    /* '|' 를 통해서 여러개의 필더 등록 */
+    template: `<li>{{ item.text }} = {{ item.createdAt | datetime }}
     <button @click="deleteClicked">X</button></li>`,
     props: {
         item: {
